c78dd26e724a0423f42edc53c033a6a21bbf3025
Sean Doyle <sdoylelambda@gmail.com>
2020-01-23 11:09:45 -0500

    UPER framework, understand, plan.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,60 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
+# SAMPLE INPUT
+recipes = {
+  'eggs': 5,
+  'butter': 10,
+  'sugar': 8,
+  'flour': 15
+}
+
+ingredients = {
+  'eggs': 5,
+  'butter': 10,
+  'sugar': 8,
+  'flour': 15
+}
+# EXPECTED SAMPLE RETURN === 0
+
+def recipe_batches(recipes, ingredients):
+  # UPER
+  # - UNDERSTAND
+  # TAKE IN RECIPE AND INGREDIENTS DICTIONARIES
+  # SEE HOW MANY DISHES CAN BE MADE FROM COMPARING INPUTS
+  # RETURN INT FOR HOW MANY DISHES CAN BE MADE
+  # P - PLAN
+  # compare each position of the dictionary(a) to the next dictionary(b)
+  #   return a // b
+  #   do for each item in dictionary
+  #   final return smallest number from above
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  for recipe in recipes:
+    r = recipe
+    print(r)
+    print(recipes.get(r))
+
+
+recipe_batches(recipes, ingredients)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PUT BACK IN TO RUN TESTS
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,35 +4,36 @@ import argparse
 
 #   track of the `current_min_price_so_far` and the `max_profit_so_far`?
 
-prices = [10, 7, 5, 8, 11, 9]
-# return 6     === (11 - 5)
-print('prices in', prices)
+# prices = [10, 7, 5, 8, 11, 9]
+# # return 6     === (11 - 5)
+# print('prices in', prices)
 
 def find_max_profit(prices):
   # take in list, find largest difference between numbers and return it
   maxPrice = max(prices)
   current_min_price_so_far = 0;
   max_profit_so_far = 0;
-  print('maxPrice', maxPrice)
+  # print('maxPrice', maxPrice)
   for i in prices:
-    print(i)
-    i 
-    # if i > current_min_price_so_far:
-    #   current_min_price_so_far = i
-    # elif
+    # print(i)
+    for x in prices:
+      # print(x)
+      if i - x > current_min_price_so_far:
+        current_min_price_so_far = i - x
+        # print('current_min_price_so_far', current_min_price_so_far)
 
-  return prices
+  return current_min_price_so_far
 
-find_max_profit(prices)
+# find_max_profit(prices)
 
 
 
 # *********** UNCOMMENT THIS OUT TO RUN TESTS ************
 
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
-#
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
+
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
